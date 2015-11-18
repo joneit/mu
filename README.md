3fb26bf5340f0e51c516b6e6bab6db15f25aebe2
Jonathan Eiten <jonathan@openfin.co>
2015-11-18 18:41:54 -0500

    Initial commit, continued

---
+++ README.md
@@ -0,0 +1,22 @@
+# µ lib
+mu - Mini Underscore library
+
+Just the object iterators, similar in name and function to those found in underscore.js.
+
+## API documentation
+
+Detailed API docs can be found [here](http://joneit.github.io/mu/global.html).
+
+Or see the Underscore docs:
+
+* [each](http://underscorejs.org/#each)
+* [find](http://underscorejs.org/#find)
+* [reduce](http://underscorejs.org/#reduce)
+* [extend](http://underscorejs.org/#extend)
+* [extendOwn](http://underscorejs.org/#extendOwn)
+
+Just keep in mind that these are for objects only (not arrays).
+
+## Note
+See the note [Regarding submodules](https://github.com/openfin/rectangular#regarding-submodules)
+for important information on cloning this repo or re-purposing its build template. (The submodule is for jsdocs support.)
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var gulp = require('gulp'),
+    $$   = require('gulp-load-plugins')();
+
+var runSequence = require('run-sequence'),
+    exec        = require('child_process').exec,
+    path        = require('path');
+
+var srcDir  = './src/',
+    testDir = './test/',
+    jsDir   = srcDir + 'js/',
+    jsFiles = '**/*.js',
+    destDir = './';
+
+var js = {
+    dir   : jsDir,
+    files : jsDir + jsFiles
+};
+
+//  //  //  //  //  //  //  //  //  //  //  //
+
+gulp.task('lint', lint);
+gulp.task('test', test);
+gulp.task('doc', doc);
+gulp.task('rootify', rootify);
+
+gulp.task('build', function(callback) {
+    clearBashScreen();
+    runSequence('lint', 'test', 'doc', 'rootify',
+        callback);
+});
+
+gulp.task('watch', function () {
+    gulp.watch([srcDir + '**', testDir + '**'], ['build']);
+});
+
+gulp.task('default', ['watch', 'build']);
+
+//  //  //  //  //  //  //  //  //  //  //  //
+
+function lint() {
+    return gulp.src(js.files)
+        .pipe($$.excludeGitignore())
+        .pipe($$.eslint())
+        .pipe($$.eslint.format())
+        .pipe($$.eslint.failAfterError());
+}
+
+function test(cb) {
+    return gulp.src(testDir + 'index.js')
+        .pipe($$.mocha({reporter: 'spec'}));
+}
+
+function doc(cb) {
+    exec(path.resolve('jsdoc.sh'), function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+}
+
+function rootify() {
+    return gulp.src(js.dir + 'mu.js')
+        .pipe($$.rename('index.js'))
+        .pipe(gulp.dest(destDir));
+}
+
+function clearBashScreen() {
+    var ESC = '\x1B';
+    console.log(ESC + 'c'); // (VT-100 escape sequence)
+}
+
